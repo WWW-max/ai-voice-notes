4563e3b009741e1ff7bca222640e4afc36e04e03
monster-wu <464711201@qq.com>
2024-07-29 18:41:30 +0800

    feat:快捷键cmd+shift+alt+n自动创建md文件同时打开语音输入

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const path = require('path');
+const fs = require('fs');
+import dayjs from 'dayjs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +16,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ai-voice-note.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	const disposable = vscode.commands.registerCommand('extension.takeNoteByVoice', () => {
+		// 拼接文件名
+        const fileName = `notes_${dayjs().format('YYYY-MM-DD HH:mm:ss')}.md`;
+        // 获取文件夹路径
+        const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+        if (workspaceFolder) {
+            // 为新文件创建绝对路径
+            const filePath = path.join(workspaceFolder.uri.fsPath, fileName);
+
+            // 创建文件并写入内容
+            fs.writeFileSync(filePath, '');
+
+            // 编辑器中打开文件
+            vscode.workspace.openTextDocument(filePath).then(doc => {
+                
+                vscode.window.showTextDocument(doc);
+
+            }, (error) => {
+                vscode.window.showErrorMessage(`Failed to open ${fileName}: ${error}`);
+            }).then(() => {
+                 // 执行开启语音输入
+                 vscode.commands.executeCommand('workbench.action.editorDictation.start');
+            });
+        } else {
+            vscode.window.showWarningMessage('No workspace opened to create note file.');
+        }	
 	});
 
 	context.subscriptions.push(disposable);
